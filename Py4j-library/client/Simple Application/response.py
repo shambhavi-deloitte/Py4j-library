fcf514f784d62b13ad74c556728c9d4bd80667bb
Shambhavi Priya <shampriya@deloitte.com>
2022-11-15 18:56:32 +0530

    initial commit

---
+++ Py4j-library/client/Simple Application/response.py
@@ -0,0 +1,12 @@
+from py4j.java_gateway import JavaGateway
+
+
+def Add_2_Num(num1, num2):
+    num1 = int(num1)
+    num2 = int(num2)
+    gateway = JavaGateway()  # connect to the JVM    
+    # addition_app = gateway.entry_point  # get the AdditionApplication instance
+    value = gateway.entry_point.addition(num1,num2) # call the addition method
+    return value
+
+print(Add_2_Num(2,3))
